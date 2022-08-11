1ab24183acc95fcbefeae29da9779bb7db71fb2e
bwiggs42 <111062531+bwiggs42@users.noreply.github.com>
2022-08-11 22:40:18 +0800

    Other new files?

--- offline.js
+++ offline.js
@@ -1,9 +1,9 @@
 ﻿{
-	"version": 1641086871,
+	"version": 1660228656,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
-		"jquery-2.1.1.min.js",
+		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/backgroundtile.png",
 		"images/playerbox-sheet0.png",
